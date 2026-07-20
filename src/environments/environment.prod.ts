2980e3d3307a7a3e6d3f2ae41b9f6c1f882a0909
JuanOrozco46 <juanma4647@gmail.com>
2026-07-19 23:56:09 -0500

    act

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'https://bpmbrrxpuodoh.supabase.co',
-    anonKey: 'sb_publishable_RQSoXFoIm57OHKqwT3SVrw_srXRfQuD'
+    url: 'https://pcmcfhhbpmbrrxpuodoh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjbWNmaGhicG1icnJ4cHVvZG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MTYwNDcsImV4cCI6MjEwMDA5MjA0N30.nTTSxgA-Lhpl85jGZ1itrTwvbSwa5APZeZOKMvYpiQQ'
   }
 };
